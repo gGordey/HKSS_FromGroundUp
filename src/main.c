2108aa71e4b8ad9ddd7f87a316fedaea785bb44d
gGordey <ggordeymini@gmail.com>
2025-08-01 02:26:09 +0300

    add glad

--- src/main.c
+++ src/main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
+#include <glad/glad.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
 int main(int argc, char **argv) {
 	printf("Hello, 'Silksong Form Grounnd Up'!\n");
-
+	
     if (!glfwInit()) {
 		char *ErrorLogBuf;
 		int glfwErrorCode = glfwGetError((const char**)&ErrorLogBuf);
-
-		fprintf(stderr, ErrorLogBuf);
+	
+		fprintf(stderr,"Not init glfw for some reason, here is an error btw: %s\n", ErrorLogBuf);
 		
 		glfwTerminate();
 		return 0;
@@ -20,6 +22,11 @@ int main(int argc, char **argv) {
 	if(window == NULL) {
 		fprintf(stderr, "No GLFW window for you tooday, GIT GUT");
 		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		fprintf(stderr, "Looks like you are not getting GLAD today, but tommorow for shaw!\n");
 	}
 
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0) {
